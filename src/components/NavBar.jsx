6e0f0ede9f33177fb905823a68445cea883bcca9
mehboob-ali <mehboobalishaikh50@gmail.com>
2022-11-27 11:52:58 +0530

    minor changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <div className=' p-2 px-4 sm:py-3 sm:px-8 justify-center items-center  flex-row flow-root '>
         <h1 className=' text-2xl sm:text-3xl font-bold font-mono float-left  text-teal-dark '>Shopping Cart</h1>
-          <AiOutlineShoppingCart className=' text-teal-dark h-10 w-10 sm:h-12 sm:w-12 float-right '></AiOutlineShoppingCart>
+          <AiOutlineShoppingCart className=' text-teal-dark h-10 w-10 sm:h-12 sm:w-12 lg:h-16 lg:w-12 float-right '></AiOutlineShoppingCart>
     </div>
   )
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -15,20 +15,22 @@ function Product() {
     console.log("data is s s  s", products)
 
   return (
-    <div className=' px-4 sm:px-40 p-2  grid grid-cols-1 gap-3
+    <div className=' px-4 sm:px-8 lg:px-36 p-2  grid grid-cols-1 gap-3 sm:grid-cols-2
          lg:grid-cols-3 lg:gap-4
     '>
         {
             products.map((product)=>(
         <div key={product.id} className=' p-2 flex-row grid grid-cols-2 text-teal-dark
+                sm:grid-cols-1
             justify-center items-center bg-teal-light rounded-2xl'>
-            <img src={product.image} className=' object-scale-down h-48 w-96 p-2'></img>
+            <img src={product.image} className=' object-scale-down h-36 w-96 p-2
+                lg:h-48 lg:w-96  '></img>
             
             <span className=' p-4 text-lg sm:text-lg text-center'>
                 {product.title}
             </span>
             <span className=' text-xl p-4 text-center font-bold'>
-                {product.price} $
+                $ {product.price}
             </span>
             <button className=' border-2 p-3 font-bold rounded-lg border-teal-dark hover:bg-teal-dark hover:text-teal-light hover:'>Add to Cart</button>
         </div>
